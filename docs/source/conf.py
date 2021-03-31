d442ebc4b7d35c82fa02e3ca74e0fd4a4f09328a
bwoodsend <bwoodsend@gmail.com>
2021-03-31 15:37:36 +0100

    Apply Tomial docs theme extension.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -40,6 +40,7 @@ extensions = [
     'sphinx.ext.autosectionlabel',
     'sphinx.ext.intersphinx',
     'sphinx_autodoc_typehints',
+    'tomial_rtd_theme',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -114,11 +115,7 @@ html_theme = "sphinx_rtd_theme"
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['static']
-
-
-def setup(app):
-    app.add_css_file('theme-overrides.css')
+html_static_path = []
 
 #html_favicon = 'favicon.png'
 
